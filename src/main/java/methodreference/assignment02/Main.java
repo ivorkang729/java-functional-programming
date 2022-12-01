5fba11f7ddd5fa3e930f759d246d702db364e25f
Oliver Kang <ivorkang729@gmail.com>
2022-12-02 00:32:11 +0800

    update

---
+++ src/main/java/methodreference/assignment02/Main.java
@@ -0,0 +1,23 @@
+package methodreference.assignment02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// 5 + 2 - 3 * 7 + 2 / 3
+		int onScreenNumber = 5;
+		onScreenNumber = calculate(onScreenNumber, MathOperation.add(), 2);
+		onScreenNumber = calculate(onScreenNumber, MathOperation.substract(), 3);
+		onScreenNumber = calculate(onScreenNumber, MathOperation.multiply(), 7);
+		onScreenNumber = calculate(onScreenNumber, MathOperation.add(), 2);
+		onScreenNumber = calculate(onScreenNumber, MathOperation.divide(), 3);
+		
+		System.out.println(onScreenNumber);
+
+	}
+	
+	static Integer calculate(int onScreenNumber, MathOperation operation, int enteredNumber) {
+		return operation.operate(onScreenNumber, enteredNumber);
+	}
+
+}
